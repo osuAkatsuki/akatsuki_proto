d51aecae10ba170b41546cd52779e8ac6bccb8f0
tsunyoku <tsunyoku@gmail.com>
2024-02-18 02:38:38 +0000

    add protobuf requirement, increment package version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name="akatsuki_proto",
-    version="0.1.0",
+    version="0.1.1",
     author="Akatsuki",
     description="Protobuf files & source generation for Akatsuki",
     packages=setuptools.find_packages(),
@@ -12,6 +12,7 @@ setup(
         package: ["*.pyi", "**/*.pyi"]
         for package in setuptools.find_packages()
     },
+    install_requires=["protobuf"],
     include_package_data=True,
     python_requires=">=3.9",
 )
